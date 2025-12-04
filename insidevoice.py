af43534780d2cfaab25277384dc2b68a93f0fdd0
Dalaho <camjs98@gmail.com>
2025-12-03 23:07:03 -0700

    Delete insidevoice.py

--- insidevoice.py
+++
@@ -1,6 +0,0 @@
-username = input("Welcome to the library, what is your name?: ")
-loudname = username.upper()
-print(loudname)
-book = input("Stop yelling! What book would you like?: ")
-quiet = book.lower
-print("ahh, thank you for lowering your voice. I will go snag",book,"right now")
